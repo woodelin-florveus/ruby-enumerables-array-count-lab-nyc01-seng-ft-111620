3ae66a4406fab174a55816aedacc86aaf08aaaa8
Woodelin Florveus <woodelinflorveus@gmail.com>
2020-10-29 22:18:01 +0000

    Automatically backed up by Learn

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -10,6 +10,7 @@ def count_empty_strings(array)
   # Return the total number of EMPTY strings in the provided array using the count enumerable
 
   array.count do |ele|
-    ele == ""
+    # ele == ""
+    ele.class == String && empty?
   end
 end
