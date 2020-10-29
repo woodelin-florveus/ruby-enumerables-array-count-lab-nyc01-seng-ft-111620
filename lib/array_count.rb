9e4105928594aaec00add6353aad679aac79f522
Woodelin Florveus <woodelinflorveus@gmail.com>
2020-10-29 22:17:22 +0000

    Automatically backed up by Learn

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -2,9 +2,7 @@ def count_strings(array)
   # Return the total number of strings in the provided array using the count enumerable
 
   array.count do |ele|
-    if ele.class == String
-      ele
-    end
+    ele.class == String
   end
 end
 
